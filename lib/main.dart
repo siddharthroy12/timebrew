9cffc9b84e88e9daaa39b1cb76fe628bb859176e
Siddharth Roy <siddharthroy36912@gmail.com>
2023-11-10 19:55:31 +0530

    Added light mode and dark mode theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,63 @@ import 'package:flutter/gestures.dart';
 import 'package:timebrew/widgets/restart.dart';
 import 'tabs/tabs.dart';
 
+// Fictitious brand color.
+const _brandBlue = Color(0xFF1E88E5);
+
+ThemeData mixWithCommonTheme(ColorScheme colorSheme) {
+  return ThemeData(
+    colorScheme: colorSheme,
+    useMaterial3: true,
+    visualDensity: VisualDensity.compact,
+    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    cardTheme: CardTheme(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+    ),
+    timePickerTheme: const TimePickerThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+    ),
+    datePickerTheme: const DatePickerThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+    ),
+    dialogTheme: const DialogTheme(
+      actionsPadding: EdgeInsets.only(
+        bottom: 10,
+        right: 10,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      isDense: true,
+      contentPadding: EdgeInsets.symmetric(
+        vertical: 18,
+        horizontal: 15,
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide(width: 2),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 2,
+        ),
+      ),
+    ),
+  );
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -26,64 +83,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          // On Android S+ devices, use the provided dynamic color scheme.
+          // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
+          lightColorScheme = lightDynamic.harmonized();
+
+          // Repeat for the dark color scheme.
+          darkColorScheme = darkDynamic.harmonized();
+        } else {
+          // Otherwise, use fallback schemes.
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: _brandBlue,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: _brandBlue,
+            brightness: Brightness.dark,
+          );
+        }
+
         return RestartWidget(
           child: MaterialApp(
             title: 'Timebrew',
             debugShowCheckedModeBanner: false,
             scrollBehavior: ThisShouldBeDefaultScrollBehavior(),
-            theme: ThemeData(
-              colorScheme: darkDynamic,
-              useMaterial3: true,
-              visualDensity: VisualDensity.compact,
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              cardTheme: CardTheme(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              timePickerTheme: const TimePickerThemeData(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5),
-                  ),
-                ),
-              ),
-              datePickerTheme: const DatePickerThemeData(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5),
-                  ),
-                ),
-              ),
-              dialogTheme: const DialogTheme(
-                actionsPadding: EdgeInsets.only(
-                  bottom: 10,
-                  right: 10,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5),
-                  ),
-                ),
-              ),
-              inputDecorationTheme: InputDecorationTheme(
-                isDense: true,
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 18,
-                  horizontal: 15,
-                ),
-                border: const OutlineInputBorder(
-                  borderSide: BorderSide(width: 2),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: darkDynamic?.primary ??
-                        const ColorScheme.dark().primary,
-                    width: 2,
-                  ),
-                ),
-              ),
-            ),
+            themeMode: ThemeMode.dark,
+            theme: mixWithCommonTheme(lightColorScheme),
+            darkTheme: mixWithCommonTheme(darkColorScheme),
             home: ContextMenuOverlay(
               buttonBuilder: (context, config, [style]) => MenuItemButton(
                 onPressed: config.onPressed,
@@ -121,5 +149,35 @@ class MyApp extends StatelessWidget {
         );
       },
     );
+  }
+}
+
+@immutable
+class CustomColors extends ThemeExtension<CustomColors> {
+  const CustomColors({
+    required this.danger,
+  });
+
+  final Color? danger;
+
+  @override
+  CustomColors copyWith({Color? danger}) {
+    return CustomColors(
+      danger: danger ?? this.danger,
+    );
+  }
+
+  @override
+  CustomColors lerp(ThemeExtension<CustomColors>? other, double t) {
+    if (other is! CustomColors) {
+      return this;
+    }
+    return CustomColors(
+      danger: Color.lerp(danger, other.danger, t),
+    );
+  }
+
+  CustomColors harmonized(ColorScheme dynamic) {
+    return copyWith(danger: danger!.harmonizeWith(dynamic.primary));
   }
 }
